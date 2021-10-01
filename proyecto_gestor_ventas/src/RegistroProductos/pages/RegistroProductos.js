8391ff587b06b8915c8f912103d01339a9df0f3e
Nataly Jaramillo <nataly0914@hotmail.com>
2021-10-01 18:35:04 -0500

    Ajuste a la interfaz Registro de Productos

--- proyecto_gestor_ventas/src/RegistroProductos/pages/RegistroProductos.js
+++ proyecto_gestor_ventas/src/RegistroProductos/pages/RegistroProductos.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import "./registroProducto.css";
 
 const RegistroProductos = () => {
 
     return (
-        <div>
+    <div className="ContenedorRP" > 
             <main>
-    
       
-           <h1>REGISTRO DE PRODUCTO</h1>          
+           <h1>Registro de Productos</h1>          
          
     <form >
         <div class="ident">
@@ -16,34 +16,34 @@ const RegistroProductos = () => {
         </div>
 
         <div class="ident">
-            <label for="produc" >PRODUCTO</label>
+            <label for="produc" >Producto</label>
             <input type="text" id="produc"/>
         </div>
 
         <div class="ident">
-            <label for="descrip" >DESCRIPCION</label>
+            <label for="descrip" >Descripción</label>
             <input type="text" id="descproduc"/>
         </div>
 
         <div class="ident">
-            <label for="valorunit" >VALOR UNITARIO</label>
+            <label for="valorunit" >Valor Unitario ($)</label>
             <input type="text" class="id" id="valorunit"/>
         </div>
         
         <div class="ident">
-            <label>ESTADO</label>
+            <label>Estado del producto</label>
             <select>
-                 <option selected disabled>Seleccione una opcion</option>                
+                 <option selected disabled>Seleccione una opción</option>                
                  <option>Disponible</option>
                  <option>No disponible</option>
             </select>
         </div>
 
         <div class="boton">
-            <button>Agregar</button>
+            <button variant="primary">Agregar producto</button>
             
 
-        </div>
+    </div>
         
         
     </form>
