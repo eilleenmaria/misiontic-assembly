028461ad404783da5ead8a9b82dd79c87196c621
eilleenmaria <egutierrezacosta85@gmail.com>
2021-10-01 14:11:22 -0500

    adicion interfaz registro de producto y listado de ventas

--- proyecto_gestor_ventas/src/ListadoVentas/pages/ListadoVentas.js
+++ proyecto_gestor_ventas/src/ListadoVentas/pages/ListadoVentas.js
@@ -1,9 +1,72 @@
+import React from 'react';
+import "./listadoventas.css";
+import eliminar from './eliminar.png';
+import editar from './editar.png';
+import buscar from './buscar.png';
+
 const ListadoVentas = () => {
 
-    return <h1> 
-        Esta sería la interfaz de listado de ventas
-    </h1>
+    return (
+        <main id="container">
+        <h1>LISTADO DE VENTAS</h1>
+        <form action="" method="get" class="form-search">
+            <input type="text" name="busqueda" id="busqueda" placeholder="Id Venta"/>
+            <button type="submit"  class="btn-search">< img src={buscar}/></button>
+        </form>
+        <div>
+            <h5>Listar por fecha</h5>
+            <form action="" method="get" class="form-search-date">
+                <label>De:</label>
+                <input type="date" name="fecha-de" id="fecha-de" required/>
+                <label>A</label> 
+                <input type="date" name="fecha-a" id="fecha-a" required/>
+                <button type="submit" class="btn-view"><img src={buscar}/></button>
 
-}; 
+            </form>
+        </div>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Fecha</th>
+                <th>Cliente</th>
+                <th>Vendedor</th>
+                <th>Estado</th>
+                <th class="textrigth">Total Factura</th>
+                <th class="textright">Acciones</th>           
+
+            </tr>
+            <tr>
+            <td>1</td>
+                <td>2022-10-01</td>
+                <td>Melanyn Grces</td>
+                <td>Pedro Perez</td>
+                <td>En proceso</td>
+                <td>$200.000</td>
+                <td>
+                    <button class="btn-edit-venta">< img src={editar}/></button> 
+                    <button class="btn-delete-venta">< img src={eliminar}/></button> 
+                    
+                </td>
+               
+            </tr>
+        </table>
+        <div class="paginador">
+            <ul>
+                <li><a href="#">|&lt;</a></li>
+                <li><a href="#">|&lt;&lt;</a></li>
+                <li class="pageSelected">1</li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;|</a></li>
+                <li><a href="#">&gt;&gt;|</a></li>
+            </ul>
+        </div>
+    </main>
+        
+    )
+
+} 
 
 export default ListadoVentas;
--- proyecto_gestor_ventas/src/RegistroProductos/pages/RegistroProductos.js
+++ proyecto_gestor_ventas/src/RegistroProductos/pages/RegistroProductos.js
@@ -1,8 +1,59 @@
+import React from 'react';
+import "./registroProducto.css";
 const RegistroProductos = () => {
 
-    return <h1> 
-        Esta sería la interfaz de registro de productos
-    </h1>
-}; 
+    return (
+        <div>
+            <main>
+    
+      
+    <h1>REGISTRO DE PRODUCTO</h1>             
+         
+    <form >
+        <div class="ident">
+            <label for="IDproduc">ID</label>
+            <input type="text" class="id" id="IDproduct"/> 
+        </div>
+
+        <div class="ident">
+            <label for="produc" >PRODUCTO</label>
+            <input type="text" id="produc"/>
+        </div>
+
+        <div class="ident">
+            <label for="descrip" >DESCRIPCION</label>
+            <input type="text" id="descproduc"/>
+        </div>
+
+        <div class="ident">
+            <label for="valorunit" >VALOR UNITARIO</label>
+            <input type="text" class="id" id="valorunit"/>
+        </div>
+        
+        <div class="ident">
+            <label>ESTADO</label>
+            <select>
+                 <option selected disabled>Seleccione una opcion</option>                
+                 <option>Disponible</option>
+                 <option>No disponible</option>
+            </select>
+        </div>
+
+        <div class="boton">
+            <button>Agregar</button>
+            <button>Eliminar</button>
+
+        </div>
+        
+        
+    </form>
+
+
+</main> 
+
+        </div>
+    )
+   
+};
 
 export default RegistroProductos;
