dcbf2ed8bb1e981c520ef75fe674917b60e63d54
Nataly Jaramillo <nataly0914@hotmail.com>
2021-10-12 20:02:34 -0500

    avance clase de hoy

--- proyecto_gestor_ventas/src/api.js
+++ proyecto_gestor_ventas/src/api.js
@@ -1,5 +1,11 @@
-const callApi = async (url) => {
-    const response = await fetch("http://localhost3001/api" + url);
+const callApi = async (url, options ={}) => {
+    
+    options.headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    };
+    
+    const response = await fetch("http://localhost3001/api" + url, options);
     const data = await response.json();
     return data;
 }
@@ -9,8 +15,11 @@ const api = {
         list() {
             return callApi("/productos");
         },
-        create() {
-            return callApi("/productos");
+        create(producto) {
+            return callApi("/productos", {
+                method: "POST", 
+                body: JSON.stringify(producto),
+            });
         },
     },
 };
